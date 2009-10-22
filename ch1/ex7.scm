1f1075623008619aef39c4f4dd73962d723b1d45
shogo <s.ngskx615@gmail.com>
2009-10-22 22:47:45 +0900

    test p

--- ch1/ex7.scm
+++ ch1/ex7.scm
@@ -6,15 +6,11 @@
 
 (define (sqrt x) 
   (define (sqrt-iter guess)
-
     (define (good-enough?)
       (< (abs (- (square guess) x)) (/ x 1000)))
-    
     (define (improve)
       (average guess (/ x guess)))
-    
     (if (good-enough?) 
 	guess
 	(sqrt-iter (improve))))
-  
   (sqrt-iter 1.0))
